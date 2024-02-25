cb0aad4e7b1e8466844bff83052391c556f01ab6
jeongmingong <jmgong59@gmail.com>
2024-02-26 00:49:22 +0900

    :tada: Init: 프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+  return <div>hello worlㅇㅇㅇd</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+    const prod = argv.mode === "production";
+    
+    return {
+        mode: prod ? "production" : "development",
+        devtool: prod ? "hidden-source-map" : "eval",
+        entry: "./src/index.tsx",
+        output: {
+            path: path.join(__dirname, "/dist"),
+            filename: "[name].js",
+        },
+        devServer: {
+            port: 3000,
+            hot: true,
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        module: {
+            rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["babel-loader", "ts-loader"],
+            },
+            ],
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                React: "react",
+            }),
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                minify: process.env.NODE_ENV === 'production' ? {
+                    collapseWhitespace: true, // 빈칸 제거
+                    removeComments: true, // 주석 제거
+                } : false,
+            }),
+            new CleanWebpackPlugin(),
+        ],
+    }
+};
