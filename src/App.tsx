1a6ab03b1316fb169520ca34f4646ec684b2da99
jeongmingong <jmgong59@gmail.com>
2024-02-26 23:55:31 +0900

    :construction_worker: Chore: router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
 const App: React.FC = () => {
-  return <div>hello worlㅇㅇㅇd</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/common/NavBar';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <NavBar />
+      <div>This Route is HomePage</div>
+    </div>
+  );
+};
+
+export default HomePage;
