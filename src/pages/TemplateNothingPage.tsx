9530c1aa493e36499bd8d434fe94d111cda473a4
jeongmingong <jmgong59@gmail.com>
2024-02-29 01:12:03 +0900

    :construction_worker: Chore: 라우터 문제 해결

---
+++ src/pages/TemplateNothingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TeamRetroCreateButton from '../components/createButton/TeamRetroCreateButton';
+
+const TemplateNothingPage: React.FC = () => {
+  return (
+    <>
+      <div>아무것도 없는 페이지</div>
+      <TeamRetroCreateButton />
+    </>
+  );
+};
+
+export default TemplateNothingPage;
