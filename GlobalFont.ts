64833c0bb9243c16ec1aa26a034eff2ca9ce8e53
heejung0413 <acb4287@gmail.com>
2024-02-28 19:05:54 +0900

    :sparkles: Feat: 프리텐다드 폰트 적용

---
+++ GlobalFont.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import GlobalStyle from './styles/fonts/GlobalStyle';
+import GlobalFont from './styles/fonts/GlobalFont';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
-    </Router>
+    <>
+      <GlobalFont />
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
