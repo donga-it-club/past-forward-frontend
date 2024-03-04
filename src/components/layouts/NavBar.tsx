3991de12622f27c17c0800d8c66312a055636f9f
heejung0413 <acb4287@gmail.com>
2024-03-04 16:20:41 +0900

    #15 ✨Feat:  네비게이션바 오른쪽 아이콘, 로그아웃 버튼 기능 추가

--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
 import styled from 'styled-components';
+import { Gear, Bell, PersonCircle } from 'react-bootstrap-icons';
 
-const Box = styled.div`
+const NavBar = () => {
+  return (
+    <Container>
+      <LeftBox>
+        <Link href='#'>Home</Link>
+        <Link href='#'>Template</Link>
+        <Link href='#'>About us</Link>
+        <Link href='#'>Contact</Link>
+        <OrdinaryButton>Create</OrdinaryButton>
+      </LeftBox>
+
+      <RightBox>
+        <IconStyle borderRadius='10px'>
+          <div style={{ display: 'flex' }}>
+            <PersonCircle style={{ padding: '5px' }} />
+            <p style={{ alignItems: 'center' }}>Clayton Santos</p>
+          </div>
+        </IconStyle>
+        <IconStyle borderRadius='45%'>
+          <Gear />
+        </IconStyle>
+        <IconStyle borderRadius='50%'>
+          <Bell />
+        </IconStyle>
+        <Link href='#'>Logout</Link>
+      </RightBox>
+    </Container>
+  );
+};
+
+export default NavBar;
+
+const Container = styled.div`
+  display: flex;
   width: 100%;
   margin: 30px;
   position: sticky;
   display: flex;
   align-items: center;
+`;
+
+const LeftBox = styled.div`
+  flex: auto;
+  text-align: center;
+`;
+
+const RightBox = styled.div`
+  flex: auto;
+  flex-direction: row;
+  text-align: center;
+`;
+
+const IconStyle = styled.div<{ borderRadius: string }>`
+  border-radius: ${(props) => props.borderRadius};
+  display: inline-block;
+  border: 2px solid black;
+  padding: 5px;
+  margin: 5px;
 `;
 
 const OrdinaryButton = styled.button`
@@ -17,34 +69,11 @@ const OrdinaryButton = styled.button`
   border-radius: 3px;
   font-size: 20px;
   padding: 10px;
+  border-radius: 15%;
 `;
 
 const Link = styled.a`
   text-decoration-line: none;
   color: #505f98;
-  margin: 10px 20px;
+  margin: 10px;
 `;
-
-const LogoText = styled.a`
-  padding: 10px;
-  border: 20px;
-  color: #111b47;
-  font-size: 30px;
-  font-weight: bold;
-`;
-
-const NavBar: React.FC = () => {
-  return (
-    <Box>
-      <img src='public/logo.png' />
-      <LogoText>Past Forward</LogoText>
-      <Link href='#'>Home</Link>
-      <Link href='#'>Template</Link>
-      <Link href='#'>About us</Link>
-      <Link href='#'>Contact</Link>
-      <OrdinaryButton>Create</OrdinaryButton>
-    </Box>
-  );
-};
-
-export default NavBar;
