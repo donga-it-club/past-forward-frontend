50289058e2d3c374720374f62404245f17faae82
공정민 <102538362+jeongmin59@users.noreply.github.com>
2024-03-06 23:21:34 +0900

    #7 :lipstick: Design: Create 모달 UI 완성 (#19)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import NavBar from '../components/common/NavBar';
 
 const HomePage: React.FC = () => {
   return (
     <div>
-      <NavBar />
       <div>This Route is HomePage</div>
     </div>
   );
