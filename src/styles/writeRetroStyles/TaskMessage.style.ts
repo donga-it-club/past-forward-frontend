0423a4423a00b7c1842a79f2a9fffabc238f6a5e
sunflower888 <84950772+sunflower888@users.noreply.github.com>
2024-03-15 01:10:02 +0900

    :sparkles: Feat: Task 컴포넌트 추가 (#38) (#48)
    Co-authored-by: geumbin <geumbin1204@gmail.com>

---
+++ src/styles/writeRetroStyles/TaskMessage.style.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const TaskMessageStyle = styled.div`
+  width: 311px;
+  height: auto;
+  padding: 6px;
+  position: absolute;
+  top: 107px;
+  border: 1px solid yellow;
+`;
+
+export const AnnounceMessageCount = styled.span`
+  font-size: 8px;
+  font-weight: 700;
+  color: #dadee5;
+  width: 57px;
+  height: 22px;
+  position: absolute;
+  top: 12px;
+  left: 7px;
+`;
+
+export const TaskMessageLine = styled.div`
+  width: 222.74px;
+  height: 0;
+  position: absolute;
+  top: 15px;
+  left: 63.76px;
+  border: 1px solid #dadee5;
+`;
