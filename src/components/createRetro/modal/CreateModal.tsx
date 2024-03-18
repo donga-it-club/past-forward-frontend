24d01935999a7c1db6f9f3bd192f7e1a47cb2566
공정민 <102538362+jeongmin59@users.noreply.github.com>
2024-03-18 12:27:45 +0900

    :lipstick: Design: CreateRetro Modal UI 퍼블리싱 완료 (#56)

---
+++ src/components/createRetro/modal/CreateModal.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import ImageUpload from '@/components/createRetro/modal/ImageUpload';
+import TitleInput from '@/components/createRetro/modal/TitleInput';
+import TemlplateSelect from '@/components/createRetro/modal/TemplateSelect';
+import StartDateCalendar from '@/components/createRetro/modal/StartDateCalender';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react';
+import * as S from '@/styles/createRetro/modal/CreateModal.style';
+
+interface CreateModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const CreateModal: React.FC<CreateModalProps> = ({ isOpen, onClose }) => {
+  const size = 'xl';
+
+  return (
+    <Modal isOpen={isOpen} size={size} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Create Retrospect</ModalHeader>
+        <ModalCloseButton />
+
+        <S.CustomModalBody>
+          <S.LeftColumn>
+            <ImageUpload />
+          </S.LeftColumn>
+          <S.RightColumn>
+            <TitleInput />
+            <TemlplateSelect />
+            <StartDateCalendar />
+          </S.RightColumn>
+        </S.CustomModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Create
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CreateModal;
--- src/pages/CreateRetroPage.tsx
+++ src/pages/CreateRetroPage.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CreateButtonBox from '@/components/createRetro/CreateButtonBox';
-import CreateModal from '@/components/createRetro/CreateModal';
 import DescriptionBox from '@/components/createRetro/DescriptionBox';
 
 const TemplateNothingPage: React.FC = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const handleCreateButtonClick = () => {
-    setModalOpen(true);
-  };
   return (
     <>
-      {modalOpen && <CreateModal onClose={() => setModalOpen(false)} />}
-      <CreateButtonBox onClick={handleCreateButtonClick} />
+      <CreateButtonBox />
       <DescriptionBox />
     </>
   );
