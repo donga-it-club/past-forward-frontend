90cd74d25cc880199afe71c5e27702349d87bbec
heejung0413 <acb4287@gmail.com>
2024-03-25 01:20:55 +0900

    🐛Fix: borderRadius -> border_radius 네임 수정 및 레이아웃 css 수정

--- src/components/layout/parts/ProfileNavBar.tsx
+++ src/components/layout/parts/ProfileNavBar.tsx
@@ -18,7 +18,7 @@ const PageNavBar = () => {
               justifyContent: 'flex-end',
             }}
           >
-            <S.IconStyle borderRadius="10px">
+            <S.IconStyle border_radius="10px">
               <div
                 style={{
                   display: 'flex',
@@ -32,10 +32,10 @@ const PageNavBar = () => {
               </div>
             </S.IconStyle>
 
-            <S.IconStyle borderRadius="45%">
+            <S.IconStyle border_radius="45%">
               <Gear />
             </S.IconStyle>
-            <S.IconStyle borderRadius="50%">
+            <S.IconStyle border_radius="50%">
               <Bell />
             </S.IconStyle>
             <S.Link href="#">Logout</S.Link>
