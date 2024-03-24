e0d0fe43fb01f994a134e30a06397d32f52708e7
heejung0413 <acb4287@gmail.com>
2024-03-25 00:42:31 +0900

    🐛Fix:  자잘한 수정 및 env.d.ts 삭제

--- src/api/interceptors/request.ts
+++ src/api/interceptors/request.ts
@@ -1,6 +1,5 @@
-import { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
-import { getErrorMessage } from '../helper';
-import { printErrorLog, printRequestLog, printResponseLog } from '@/utils/log';
+import { InternalAxiosRequestConfig } from 'axios';
+import { printRequestLog } from '@/utils/log';
 
 export function logRequest(config: InternalAxiosRequestConfig) {
   printRequestLog({
@@ -10,31 +9,6 @@ export function logRequest(config: InternalAxiosRequestConfig) {
     requestData: config.data,
     config,
   });
+
   return config;
-}
-
-export function logResponse(response: AxiosResponse) {
-  const { config, data } = response;
-
-  printResponseLog({
-    method: config?.method,
-    endPoint: config?.url,
-    responseObj: data?.data ?? data,
-  });
-
-  return response;
-}
-
-export function logAndProcessError(e: AxiosError) {
-  const url = e.config?.url;
-  const method = e.config?.method;
-
-  const errorMessage = getErrorMessage(e);
-
-  printErrorLog({
-    method,
-    endPoint: url,
-    errorMessage,
-    errorObj: e,
-  });
 }
