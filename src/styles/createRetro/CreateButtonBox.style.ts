f4246bc53f248f5e4f03f8c2bc3e7b2d6f4bac8d
jeongmingong <jmgong59@gmail.com>
2024-03-26 10:49:19 +0900

    :lipstick: Design: CreateRetro UI 수정

--- src/styles/createRetro/CreateButtonBox.style.ts
+++ src/styles/createRetro/CreateButtonBox.style.ts
@@ -7,6 +7,6 @@ export const ButtonListContainer = styled.div`
 `;
 
 export const SpacedButton = styled.div`
-  margin: 0 2rem; // 버튼 간 간격 만듦
+  margin: 0 3.5rem; // 버튼 간 간격 만듦
   cursor: pointer;
 `;
--- src/styles/createRetro/Description.style.ts
+++ src/styles/createRetro/Description.style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const DescriptionBorder = styled.div`
-  width: 23rem;
+  width: 27rem;
   line-height: 2;
   /* height: 13rem; */
 `;
