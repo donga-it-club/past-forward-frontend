90612f9322713711578feef282d7b6faa2be9f03
heejung0413 <acb4287@gmail.com>
2024-03-27 00:38:52 +0900

    🐛Fix: 필요없는 핸들러 삭제

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,17 +3,6 @@ import { setupWorker } from 'msw/browser';
 import { mockRetrospectiveTemplate } from '@/api/__mock__/retrospectiveTemplate';
 import { mockSection } from '@/api/__mock__/section';
 
-// 임시 api
-export const CommentHandlers: RequestHandler[] = [
-  http.get('/comments/get', () => {
-    const mock = {
-      id: 'acb4287',
-      name: 'string',
-    };
-    return HttpResponse.json(mock);
-  }),
-];
-
 export const RetrospectiveHandlers: RequestHandler[] = [
   http.post('/retrospectives', () => {
     const mock = {
@@ -33,4 +22,4 @@ export const SectionHandlers: RequestHandler[] = [
     return HttpResponse.json(mockSection);
   }),
 ];
-export const mswWorker = setupWorker(...CommentHandlers, ...RetrospectiveHandlers, ...SectionHandlers);
+export const mswWorker = setupWorker(...RetrospectiveHandlers, ...SectionHandlers);
