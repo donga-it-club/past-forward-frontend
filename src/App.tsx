d82eac75442db69d217ec3c5b04616a2049336aa
양윤상 (캐테) <yoonsang@catchtable.co.kr>
2024-03-30 00:30:20 +0900

    Feat: Amplify auth init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './awsConfig';
 import MainLayout from './components/layout/MainLayout';
 import InviteTeamModal from '@/components/inviteTeam/InviteTeamModal';
 import SubLayout from '@/components/layout/PageSubLayout';
@@ -10,6 +14,8 @@ import MyPage from '@/pages/MyPage';
 import RegisterPage from '@/pages/RegisterPage';
 import SurveyPage from '@/pages/SurveyPage';
 import WriteRetroTeamPage from '@/pages/WriteRetroTeamPage';
+
+Amplify.configure(awsConfig);
 
 const App = () => {
   return (
@@ -36,4 +42,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
