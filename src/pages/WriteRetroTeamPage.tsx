fca5d61adbb39ffea87392edd4baa499653fad0e
geumbin <geumbin1204@gmail.com>
2024-03-31 22:25:09 +0900

    :sparkles: Feat : commit 확인

--- src/pages/WriteRetroTeamPage.tsx
+++ src/pages/WriteRetroTeamPage.tsx
@@ -3,7 +3,6 @@ import SaveButton from '@/components/writeRetro/SaveButton';
 import SubTitle from '@/components/writeRetro/SubTitle';
 import Title from '@/components/writeRetro/Title';
 import WriteRetroActionItems from '@/components/writeRetro/WriteRetroActionItems';
-import WriteRetroKudos from '@/components/writeRetro/WriteRetroKudos';
 import WriteRetroToImprove from '@/components/writeRetro/WriteRetroToImprove';
 import WriteRetroWentWell from '@/components/writeRetro/WriteRetroWentWell';
 
@@ -14,7 +13,6 @@ function WriteRetroTeamPage() {
       <SubTitle subTitle="첫 회고 진행 - 앞으로 남은 회고는 2번!"></SubTitle>
       <SaveButton></SaveButton>
       <CheckPopUp></CheckPopUp>
-      <WriteRetroKudos></WriteRetroKudos>
       <WriteRetroWentWell></WriteRetroWentWell>
       <WriteRetroToImprove></WriteRetroToImprove>
       <WriteRetroActionItems></WriteRetroActionItems>
