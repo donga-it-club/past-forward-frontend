9a3c0591400e513d063e72edad76a490449035eb
LimHeeJung <138123134+heejung0413@users.noreply.github.com>
2024-04-03 16:13:17 +0900

    #114 [Fix] enum -> as const 수정 및 pr template 수정
    * 🐛fix: @type enum 형식에서 as const 형식으로 변경
    
    * 🐛fix: pr templaate 이슈 링크 생성
    
    * 🐛fix: Implements -> 이슈번호로 문자 수정
    
    * 🐛Fix: 이슈번호 위치 변경

--- src/api/__mock__/retrospective.ts
+++ src/api/__mock__/retrospective.ts
@@ -1,21 +1,22 @@
-import { Status } from '../@types/@enums';
-import { GetRetrospectiveResponse } from '../@types/Retrospectives';
+import { TStatus } from '../@types/@asConst';
 
-export const MockRetrospective: GetRetrospectiveResponse = {
-  code: 0,
-  message: 'okay',
-  data: {
-    totalCount: 0,
-    nodes: [
-      {
-        id: 0,
-        title: 'hee',
-        userId: 0,
-        templateId: 0,
-        status: Status.COMPLETED,
-        isBookmarked: true,
-        thumbnail: '3fa85f64',
-      },
-    ],
-  },
-};
+export function MockRetrospective(Status: TStatus) {
+  return {
+    code: 0,
+    message: 'okay',
+    data: {
+      totalCount: 0,
+      nodes: [
+        {
+          id: 0,
+          title: 'hee',
+          userId: 0,
+          templateId: 0,
+          status: Status.PREVIOUSLY,
+          isBookmarked: true,
+          thumbnail: '3fa85f64',
+        },
+      ],
+    },
+  };
+}
