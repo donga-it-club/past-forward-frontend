6cdf57155cfbb825935bfe059143d8720fa2a814
Yena Kim <76564438+yeneua@users.noreply.github.com>
2024-04-09 14:19:15 +0900

    :sparkles: Feat: Main 페이지 내비 태그 연결 (#135)

--- src/components/layout/parts/MenuBar.tsx
+++ src/components/layout/parts/MenuBar.tsx
@@ -3,16 +3,16 @@ import * as S from '@/styles/layout/layout.style';
 const MenuBar = () => {
   return (
     <S.LeftBox>
-      <S.Link href="#" id="header_home">
+      <S.Link href="#Home" id="header_home">
         Home
       </S.Link>
-      <S.Link href="#" id="header_temp">
+      <S.Link href="#Template" id="header_temp">
         Template
       </S.Link>
-      <S.Link href="#" id="header_about">
+      <S.Link href="#AboutUs" id="header_about">
         About us
       </S.Link>
-      <S.Link href="#" id="header_contact">
+      <S.Link href="#Contact" id="header_contact">
         Contact
       </S.Link>
       <S.OrdinaryButton>Create</S.OrdinaryButton>
