9d513cb078e6f992795e075726abafec167705f8
heejung0413 <acb4287@gmail.com>
2024-04-14 16:53:30 +0900

    Revert "Comment: comment api type 설정 (1차 작업)"
    This reverts commit c906c6d88661c134d93fbf75910c76d1ace238c9.

--- src/api/@types/Comment.ts
+++ src/api/@types/Comment.ts
@@ -1,48 +1,15 @@
-export interface GetCommentRequest {
+// 임시 API
+
+export interface CommentRequest {
   id: string;
 }
 
-export interface GetCommentResponse {
-  code: number;
-  message: string;
-  data: {
-    id: number;
-    content: string;
-  };
+export interface CommentResponse {
+  id: string;
+  name: string;
+  text: string;
 }
-
-//put
-export interface PutCommentRequest {
-  id: number;
-}
-
-//delete
-export interface DeleteCommentRequest {
-  id: number;
-}
-
-export interface DeleteCommentResponse {
-  code: number;
-  message: string;
-  data: object;
-}
-
-//GetAllComment
-export interface AllGetCommentResponse {
-  code: number;
-  message: string;
-  data: CommentData[];
-}
-
-export interface CommentData {
-  id: number;
-  comment: string;
-}
-
-//Post
 
 export interface CommentClient {
-  getComment(request: GetCommentRequest): Promise<GetCommentResponse>;
-  delete(request: DeleteCommentRequest): Promise<DeleteCommentResponse>;
-  getAllComment(): Promise<AllGetCommentResponse>;
+  getComment(): Promise<CommentResponse>;
 }
