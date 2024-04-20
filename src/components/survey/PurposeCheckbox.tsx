60ace793531f7bc0a0d01690896f5ad0d0cbfecc
geumbin <geumbin1204@gmail.com>
2024-04-21 04:43:24 +0900

    :sparkles: Feat : Survey API 최종 수정

--- src/components/survey/PurposeCheckbox.tsx
+++ src/components/survey/PurposeCheckbox.tsx
@@ -1,11 +1,9 @@
-
 import React, { useState, useEffect } from 'react';
 
 import { Checkbox, Stack, Input, Text } from '@chakra-ui/react';
 import * as S from '@/styles/survey/PurposeCheckbox.style';
 
 interface Purpose {
-
   onPurposeChange: (purpose: string[]) => void;
 }
 
@@ -30,7 +28,6 @@ const PurposeCheckbox: React.FC<Purpose> = ({ onPurposeChange }) => {
     let updatedPurposes: string[];
 
     if (isChecked) {
-
       updatedPurposes = [...checkedPurposes, purpose];
     } else {
       updatedPurposes = checkedPurposes.filter(item => item !== purpose);
@@ -44,7 +41,6 @@ const PurposeCheckbox: React.FC<Purpose> = ({ onPurposeChange }) => {
     const newOtherPurpose = event.target.value;
     setOtherPurpose(newOtherPurpose);
     setIsOtherSelected(!!newOtherPurpose);
-
   };
 
   return (
@@ -72,7 +68,6 @@ const PurposeCheckbox: React.FC<Purpose> = ({ onPurposeChange }) => {
               </Checkbox>
             </Stack>
             <Stack direction="row">
-
               <Checkbox colorScheme="brand" isChecked={isOtherSelected}>
                 기타
               </Checkbox>
@@ -92,44 +87,3 @@ const PurposeCheckbox: React.FC<Purpose> = ({ onPurposeChange }) => {
 };
 
 export default PurposeCheckbox;
-
-// import React from 'react';
-// import { Checkbox, Stack, Input, Text } from '@chakra-ui/react';
-// import * as S from '@/styles/survey/PurposeCheckbox.style';
-
-// interface Purpose {
-//   onPurposeChange: (city: string) => void;
-// }
-
-// const PurposeCheckbox: React.FC<Purpose> = ({ onPurposeChange }) => {
-//   const [purpose, setPurpose] = useState<string>('서울');
-//   const handlePurposeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const newPurpose = event.target.value;
-//     setPurpose(newPurpose);
-//     onPurposeChange(newPurpose);
-//   };
-
-//   return (
-//     <>
-//       <S.CustomContainer>
-//         <Text fontSize="lg">Past-Forward 서비스를 알게 된 경로는 무엇입니까?</Text>
-//         <Text>(복수 선택 가능)</Text>
-//         <S.CheckboxContainer>
-//           <Stack direction="column" spacing={6}>
-//             <Stack direction="row" spacing={3}>
-//               <Checkbox colorScheme="brand">업무 목적</Checkbox>
-//               <Checkbox colorScheme="brand">개인 발전</Checkbox>
-//               <Checkbox colorScheme="brand">팀 협업</Checkbox>
-//               <Checkbox colorScheme="brand">프로젝트 관리</Checkbox>
-//               <Checkbox colorScheme="brand">학습 및 개선</Checkbox>
-//             </Stack>
-//             <Stack direction="row">
-//               <Checkbox colorScheme="brand">기타</Checkbox>
-//               <Input width="20rem" placeholder="직접 입력해주세요." />
-//             </Stack>
-//           </Stack>
-//         </S.CheckboxContainer>
-//       </S.CustomContainer>
-//     </>
-//   );
-// };
