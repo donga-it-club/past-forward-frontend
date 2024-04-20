baadcc5b47f570afd213a167c4c4d2508e513de6
jeongmingong <jmgong59@gmail.com>
2024-04-20 15:06:19 +0900

    :bug: Fix: 사진 조회 수정 완료

--- src/styles/RetroList/ContentsList.styles.ts
+++ src/styles/RetroList/ContentsList.styles.ts
@@ -53,6 +53,8 @@ export const RetroDate = styled.span`
 export const Thumbnail = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
+  aspect-ratio: 1/1;
 `;
 
 export const TeamIcon = styled.img`
