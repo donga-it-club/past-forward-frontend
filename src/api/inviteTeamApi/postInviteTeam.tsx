4e2ad0dc971d1781f4238ddcf43c859a29003338
jeongmingong <jmgong59@gmail.com>
2024-04-21 14:31:36 +0900

    :lipstick: Design: 초대 버튼 만듦

--- src/api/inviteTeamApi/postInviteTeam.tsx
+++ src/api/inviteTeamApi/postInviteTeam.tsx
@@ -3,7 +3,7 @@ import axiosInstance from '../axiosConfig';
 
 const postInviteTeam = async (invitationId: string) => {
   try {
-    const response = await axiosInstance.post('/team/accept-invitation', {
+    const response = await axiosInstance.post('/teams/accept-invitation', {
       invitationCode: invitationId, // useParams 로 받아온 코드
     });
     console.log('팀원 초대 성공', response.data);
--- src/components/inviteTeam/AcceptInvite.tsx
+++ src/components/inviteTeam/AcceptInvite.tsx
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, Text } from '@chakra-ui/react';
 import postInviteTeam from '@/api/inviteTeamApi/postInviteTeam';
+import * as S from '@/styles/inviteTeam/AcceptInvite';
 
 const AcceptInvite: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [inviteSuccess, setInviteSuccess] = useState(false);
 
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const invitationCode = searchParams.get('invitationId');
+  const searchParams = new URLSearchParams(location.search);
+  const invitationCode = searchParams.get('invitationId');
 
-    const acceptInvitation = async () => {
-      try {
-        if (invitationCode) {
-          const response = await postInviteTeam(invitationCode);
-          // 백엔드에서 204 반환해줌
-          if (response.status === 204) {
-            setInviteSuccess(true); // 초대 요청이 성공했을 때 상태를 true로 변경
-          } else {
-            console.error('초대 수락 실패');
-          }
+  const acceptInvitation = async () => {
+    try {
+      if (invitationCode) {
+        const response = await postInviteTeam(invitationCode);
+        // 백엔드에서 204 반환해줌
+        if (response.status === 204) {
+          setInviteSuccess(true); // 초대 요청이 성공했을 때 상태를 true로 변경
         } else {
-          console.error('invitationCode 추출 실패');
+          console.error('초대 수락 실패');
         }
-      } catch (error) {
-        console.error('에러', error);
+      } else {
+        console.error('invitationCode 추출 실패');
       }
-    };
-
-    acceptInvitation();
-  }, [location.search]);
+    } catch (error) {
+      console.error('에러', error);
+    }
+  };
 
   useEffect(() => {
     if (inviteSuccess) {
@@ -40,7 +38,18 @@ const AcceptInvite: React.FC = () => {
     }
   }, [inviteSuccess, navigate]);
 
-  return <div>초대를 수락하는 중...</div>;
+  return (
+    <>
+      <S.Container>
+        <S.TextContainer>
+          <Text fontSize="2xl">초대를 수락하시겠습니까?</Text>
+        </S.TextContainer>
+        <Button colorScheme="brand" size="lg" onClick={acceptInvitation}>
+          초대 수락하기
+        </Button>
+      </S.Container>
+    </>
+  );
 };
 
 export default AcceptInvite;
