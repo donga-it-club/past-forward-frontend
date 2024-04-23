31a86dc8485e3ab7c82eff2f05ec1c7935b784ca
heejung0413 <acb4287@gmail.com>
2024-04-24 00:10:05 +0900

    💄Design: section 타이틀 부분 회고 수정하기 버튼 추가

--- src/components/writeRetro/layout/Title.tsx
+++ src/components/writeRetro/layout/Title.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react';
-import { IoIosMore } from 'react-icons/io';
 import { MdPeopleAlt } from 'react-icons/md';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Flex } from '@chakra-ui/react';
 import InviteTeamModal from '@/components/inviteTeam/InviteTeamModal';
 import * as S from '@/styles/writeRetroStyles/Layout.style';
 import * as L from '@/styles/writeRetroStyles/ReviseLayout.style';
@@ -21,30 +21,29 @@ const Title: FC<Props> = ({ name }) => {
   return (
     <>
       <S.TitleBox>
-        <div style={{ display: 'flex', flexDirection: 'column', flex: 7, margin: '20px 10px' }}>
-          <div style={{ display: 'flex' }}>
+        <Flex flexDirection="column" margin="20px 10px">
+          <Flex>
             <MdPeopleAlt size="40px" color="#434343" style={{ margin: 'auto 0' }} />
 
             <S.TitleText>{name}</S.TitleText>
+            <L.InvitationLinkButton
+              id="wr_edit"
+              style={{ backgroundColor: '#E9E9E9', color: 'black' }}
+              onClick={() => {
+                navigate(`/revise?retrospectiveId=${retrospectiveId}&teamId=${teamId}`);
+              }}
+            >
+              회고 수정하기
+            </L.InvitationLinkButton>
             <L.InvitationLinkButton onClick={() => setInviteModalOpen(true)}>팀원 초대 링크</L.InvitationLinkButton>
             <L.LinkExpirationText>링크는 2시간 후에 만료됩니다.</L.LinkExpirationText>
-          </div>
+          </Flex>
 
           <S.SubTitleText>
             첫 회고 진행 - 앞으로 남은 회고는 2번! <br />
           </S.SubTitleText>
-        </div>
+        </Flex>
 
-        <S.SaveSettingBox style={{ flex: 1 }}>
-          <S.SettingButton
-            id="wr_edit"
-            onClick={() => {
-              navigate(`/revise?retrospectiveId=${retrospectiveId}&teamId=${teamId}`);
-            }}
-          >
-            <IoIosMore size={'40px'} color="#B1B2B2" />
-          </S.SettingButton>
-        </S.SaveSettingBox>
         <InviteTeamModal isOpen={isInviteModalOpen} onClose={() => setInviteModalOpen(false)} teamId={teamId} />
       </S.TitleBox>
     </>
