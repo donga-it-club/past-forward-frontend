9798e11d496ae078f44fe703075bee76180c6058
jeongmingong <jmgong59@gmail.com>
2024-04-24 22:23:11 +0900

    :bug: Fix: 팀원 초대 수락 전 로그인 분기 처리

--- src/components/inviteTeam/AcceptInvite.tsx
+++ src/components/inviteTeam/AcceptInvite.tsx
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { getCurrentUser } from '@aws-amplify/auth';
 import { Button, Text } from '@chakra-ui/react';
 import postInviteTeam from '@/api/inviteTeamApi/postInviteTeam';
+import { useCustomToast } from '@/hooks/useCustomToast';
 import * as S from '@/styles/inviteTeam/AcceptInvite';
 
 const AcceptInvite: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [inviteSuccess, setInviteSuccess] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const toast = useCustomToast();
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  // 로그인된 상태인지 확인
+  const checkLoginStatus = async () => {
+    try {
+      await getCurrentUser();
+      setIsLoggedIn(true);
+    } catch (error) {
+      setIsLoggedIn(false);
+    }
+  };
 
   const searchParams = new URLSearchParams(location.search);
   const invitationCode = searchParams.get('invitationId');
@@ -15,12 +33,17 @@ const AcceptInvite: React.FC = () => {
   const acceptInvitation = async () => {
     try {
       if (invitationCode) {
-        const response = await postInviteTeam(invitationCode);
-        // 백엔드에서 204 반환해줌
-        if (response.status === 204) {
-          setInviteSuccess(true); // 초대 요청이 성공했을 때 상태를 true로 변경
+        if (isLoggedIn) {
+          // 로그인된 사용자인 경우에만 초대 수락 요청 보냄
+          const response = await postInviteTeam(invitationCode);
+          // 백엔드에서 204 반환해줌
+          if (response.ok) {
+            setInviteSuccess(true); // 초대 요청이 성공했을 때 상태를 true로 변경
+          } else {
+            console.error('초대 수락 실패');
+          }
         } else {
-          console.error('초대 수락 실패');
+          navigate('/login'); // 로그인되지 않은 사용자는 로그인 페이지로 리다이렉트
         }
       } else {
         console.error('invitationCode 추출 실패');
@@ -33,10 +56,10 @@ const AcceptInvite: React.FC = () => {
   useEffect(() => {
     if (inviteSuccess) {
       // 초대 성공 시 알림을 띄우고 retrolist 페이지로 navigate
-      alert('초대를 성공적으로 수락했습니다!');
+      toast.info('초대를 성공적으로 수락했습니다!');
       navigate('/retrolist');
     }
-  }, [inviteSuccess, navigate]);
+  }, [inviteSuccess]);
 
   return (
     <>
