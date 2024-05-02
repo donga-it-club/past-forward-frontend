c8fcc78fdab9dfb49849dbc2d5f553adae6edece
Yena Kim <rladPsk1117@gmail.com>
2024-05-03 03:58:42 +0900

    :bug: Fix: Action Items 담당자 지정 창 배포 전 수정 (#246)
    * 🐛fix: 필요없는 이미지 삭제
    
    * fix: navigate 오류 수정
    
    * Revert "fix: navigate 오류 수정"
    
    This reverts commit 45c720f6beae58142b71d7bcf4d8b80492f36416.
    
    * :bug: Fix: Action Items 프로필 이미지 추가 및 창 닫힘 설정 (#234)
    
    * :bug: Fix: Action Items 담당자 지정 창 프로필 이미지 추가
    
    * :bug: Fix: 담당자 눌렀을때 창 닫히게 설정
    
    * 🐛fix: inviteTeam modal localhost 수정
    
    * 🐛fix: home 디자인 수정, personal section 페이지 팀원 초대 링크 삭제
    
    * 🐛fix: default image 수정과 section 작성자 옆 간격 수정
    
    * ✨feat: 구글 애널리틱스 script 코드 작성
    
    * 🐛fix: index.html script 수정
    
    * : sparkles : Feat : Survey 질문 수정 (#242)
    
    * :bug: Fix: Action Items 프로필 이미지 추가 및 창 닫힘 설정 (#234)
    
    * :bug: Fix: Action Items 담당자 지정 창 프로필 이미지 추가
    
    * :bug: Fix: 담당자 눌렀을때 창 닫히게 설정
    
    * : sparkles : Feat : Survey 질문 수정
    
    ---------
    
    Co-authored-by: Yena Kim <76564438+yeneua@users.noreply.github.com>
    Co-authored-by: geumbin <geumbin1204@gmail.com>
    
    * Revert ": sparkles : Feat : Survey 질문 수정 (#242)" (#244)
    
    This reverts commit 55a9b89f01842ca5211e0bd653ff620bf36e92f7.
    
    * :bug: Fix: Action Items 담당자 지정 API DTO 수정
    
    * :bug: Fix: Action Items 담당자 지정 창 팀원 프로필 이미지 수정
    
    ---------
    
    Co-authored-by: 공정민 <102538362+jeongmin59@users.noreply.github.com>
    Co-authored-by: heejung0413 <acb4287@gmail.com>
    Co-authored-by: yanggak12 <yanggak12@naver.com>
    Co-authored-by: sunflower888 <84950772+sunflower888@users.noreply.github.com>
    Co-authored-by: geumbin <geumbin1204@gmail.com>

--- src/components/writeRetro/ActionItems/ActionItemTask.tsx
+++ src/components/writeRetro/ActionItems/ActionItemTask.tsx
@@ -1,7 +1,7 @@
 import { FC, useState, useEffect } from 'react';
+import { CgProfile } from 'react-icons/cg';
 import { Popover, PopoverTrigger, PopoverContent } from '@chakra-ui/react';
 import { TeamControllerServices } from '@/api/services/TeamController';
-import UserProfileImage from '@/components/user/UserProfileImage';
 import Members from '@/components/writeRetro/ActionItems/Members';
 import { useCustomToast } from '@/hooks/useCustomToast';
 import * as S from '@/styles/writeRetroStyles/Layout.style';
@@ -22,18 +22,27 @@ const ActionItemTask: FC<ActionItemTaskProps> = ({ tId, rId, sId }) => {
   const retrospectiveId: number = rId;
   const sectionId: number = sId;
 
-  const [users, setUsers] = useState<{ name: string; image: string }[]>([]);
+  const [users, setUsers] = useState<{ name: string; image: string; userId: number }[]>([]);
+  const [imageURL, setImageURL] = useState<{ url: string }[]>([]);
   const toast = useCustomToast();
 
   const fetchTeamMember = async () => {
     try {
       if (teamId) {
-        const data = await TeamControllerServices.TeamMemberGet({ teamId: teamId, retrospectiveId: retrospectiveId });
+        const data = await TeamControllerServices.TeamMemberGet({
+          teamId: teamId,
+          retrospectiveId: retrospectiveId,
+        });
         const userData = data.data.map(member => ({
           name: member.username,
           image: member.profileImage,
+          userId: member.userId,
         }));
         setUsers(userData);
+        const image = data.data.map(member => ({
+          url: member.profileImage,
+        }));
+        setImageURL(image);
       }
       return;
     } catch (e) {
@@ -43,7 +52,6 @@ const ActionItemTask: FC<ActionItemTaskProps> = ({ tId, rId, sId }) => {
   useEffect(() => {
     fetchTeamMember();
   }, []);
-
   const togglePopup = () => {
     setShowPopup(!showPopup);
   };
@@ -73,7 +81,14 @@ const ActionItemTask: FC<ActionItemTaskProps> = ({ tId, rId, sId }) => {
             onMouseEnter={() => handleMouseEnter(selectedUserName || '')}
             onMouseLeave={handleMouseLeave}
           >
-            {selectedUserImg ? <UserProfileImage width="30px" /> : 'M'}
+            {selectedUserImg ? (
+              <img src={selectedUserImg} style={{ width: '30px' }} />
+            ) : selectedUserImg === '' ? (
+              <CgProfile size="24px" color="#969696" />
+            ) : (
+              'M'
+            )}
+
             {hoveredUser && <S.HoverUser>{hoveredUser}</S.HoverUser>}
           </S.ManagerButton>
         </PopoverTrigger>
@@ -85,6 +100,7 @@ const ActionItemTask: FC<ActionItemTaskProps> = ({ tId, rId, sId }) => {
             tId={teamId}
             rId={retrospectiveId}
             sId={sectionId}
+            imageURL={imageURL}
           />
         </PopoverContent>
       </Popover>
