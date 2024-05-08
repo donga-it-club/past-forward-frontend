a23863319b5fcacf23567b91c902a6e12b7c8403
LimHeeJung <138123134+heejung0413@users.noreply.github.com>
2024-05-08 19:58:04 +0900

    [Fix] 자잘한 수정 (#261)
    * :sparkles: Feat: 설문조사 질문 구분 Id 값 입력 (#251)
    
    Co-authored-by: geumbin <geumbin1204@gmail.com>
    
    * [Fix] team member profile image (#252)
    
    * 🐛fix: 팀원 초대 도메인 수정
    
    * 🐛fix: 팀원 사진 렌더링
    
    * Revert "[Fix] team member profile image (#252)"
    
    This reverts commit 3c4296e5c33c1cdf3bdab57feef05b89bd7b51b6.
    
    * 🐛fix: default image 수정
    
    * 🐛fix: freestart 버튼 아이디값 지정
    
    * 🐛fix:  teamId 값 수정
    
    * temp: 선애님 피그마 작성
    
    * Revert "temp: 선애님 피그마 작성"
    
    This reverts commit c38fc0dadd534924fb59d11e9685d268b0472cbd.
    
    * 🐛fix: 팀 초대 링크 임시 수정
    
    * fix: 자잘한 코드 정리 수정
    
    * 🐛fix: 작성자만 회고 수정하기 보이게 및 수정 삭제 작성자만 보이게
    
    * ✨feat: spinner 기능
    
    ---------
    
    Co-authored-by: Yena Kim <rladPsk1117@gmail.com>
    Co-authored-by: 배금빈 <84950772+sunflower888@users.noreply.github.com>
    Co-authored-by: geumbin <geumbin1204@gmail.com>

--- src/components/writeRetro/layout/Title.tsx
+++ src/components/writeRetro/layout/Title.tsx
@@ -1,7 +1,10 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 import RetroTitle from './RetroTitle';
+import { RetrospectiveData } from '@/api/@types/Retrospectives';
+import { UserData } from '@/api/@types/Users';
+import postImageToS3 from '@/api/imageApi/postImageToS3';
 import InviteTeamModal from '@/components/inviteTeam/InviteTeamModal';
 import * as S from '@/styles/writeRetroStyles/Layout.style';
 import * as L from '@/styles/writeRetroStyles/ReviseLayout.style';
@@ -9,50 +12,55 @@ import * as L from '@/styles/writeRetroStyles/ReviseLayout.style';
 interface Props {
   name: string;
   description: string;
+  user: UserData;
+  retro: RetrospectiveData;
 }
 
-const Title: FC<Props> = ({ name, description }) => {
+const Title: FC<Props> = ({ name, description, retro, user }) => {
   const { search } = useLocation();
   const query = search.split(/[=,&]/);
   const retrospectiveId = Number(query[1]);
   const teamId = query[3] === 'null' ? null : Number(query[3]);
   const [isInviteModalOpen, setInviteModalOpen] = useState<boolean>(false);
-  // const [imageURL, setImageURL] = useState<{ [key: number]: string }>({});
+  const [imageURL, setImageURL] = useState<{ [key: number]: string }>({});
   const navigate = useNavigate();
 
-  // const fetchRetrospectiveImage = async (item: string) => {
-  //   if (item) {
-  //     try {
-  //       const data = await postImageToS3({ filename: item, method: 'GET' });
-  //       setImageURL(prev => ({
-  //         ...prev,
-  //         [retro.retrospectiveId]: data.data.preSignedUrl,
-  //       }));
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
-  //   }
-  // };
+  const fetchRetrospectiveImage = async (item: string) => {
+    if (item) {
+      try {
+        const data = await postImageToS3({ filename: item, method: 'GET' });
+        setImageURL(prev => ({
+          ...prev,
+          [retro.retrospectiveId]: data.data.preSignedUrl,
+        }));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchRetrospectiveImage(String(thumbnail));
-  // }, []);
+  useEffect(() => {
+    fetchRetrospectiveImage(String(retro.thumbnail));
+  }, []);
   return (
     <>
       <S.TitleBox>
-        {/* <Image src="MainPageLogo.svg" maxWidth={150} borderRadius="10px" margin="auto 5px" /> */}
+        <Image src={imageURL[retro.retrospectiveId]} maxWidth={150} borderRadius="10px" margin="auto 5px" />
         <Flex flexDirection="column" margin="20px 10px">
           <Flex>
             <RetroTitle teamId={teamId} name={name} />
-            <L.InvitationLinkButton
-              id="wr_edit"
-              style={{ backgroundColor: '#E9E9E9', color: 'black' }}
-              onClick={() => {
-                navigate(`/revise?retrospectiveId=${retrospectiveId}&teamId=${teamId}`);
-              }}
-            >
-              회고 수정하기
-            </L.InvitationLinkButton>
+            {user.userId === retro.userId && (
+              <L.InvitationLinkButton
+                id="wr_edit"
+                style={{ backgroundColor: '#E9E9E9', color: 'black' }}
+                onClick={() => {
+                  navigate(`/revise?retrospectiveId=${retrospectiveId}&teamId=${teamId}`);
+                }}
+              >
+                회고 수정하기
+              </L.InvitationLinkButton>
+            )}
+
             {teamId ? (
               <>
                 <L.InvitationLinkButton onClick={() => setInviteModalOpen(true)}>팀원 초대 링크</L.InvitationLinkButton>
