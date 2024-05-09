f05cdac59d70e3a62d8ef63949ed97718f57791e
yena <rladpsk1117@gmail.com>
2024-05-09 17:26:57 +0900

    :bug: Fix: Action Items 담당자 새로고침 시 렌더링

--- src/components/writeRetro/ActionItems/ActionItemTask.tsx
+++ src/components/writeRetro/ActionItems/ActionItemTask.tsx
@@ -1,7 +1,8 @@
 import { FC, useState, useEffect } from 'react';
 import { CgProfile } from 'react-icons/cg';
 import { Popover, PopoverTrigger, PopoverContent } from '@chakra-ui/react';
-import { sectionData } from '@/api/@types/Section';
+import { sectionData, ActionItemData } from '@/api/@types/Section';
+import postImageToS3 from '@/api/imageApi/postImageToS3';
 import { TeamControllerServices } from '@/api/services/TeamController';
 import Members from '@/components/writeRetro/ActionItems/Members';
 import { useCustomToast } from '@/hooks/useCustomToast';
@@ -15,11 +16,10 @@ interface ActionItemTaskProps {
 }
 
 const ActionItemTask: FC<ActionItemTaskProps> = ({ tId, rId, sId, section }) => {
+  const ActionItems: ActionItemData | undefined = section.actionItems;
   const [showPopup, setShowPopup] = useState<boolean>(false);
-  const [selectedUserName, setSelectedUserName] = useState<string | null>(null);
+  const [selectedUserName, setSelectedUserName] = useState<string>(ActionItems?.username || '');
   const [selectedUserImg, setSelectedUserImg] = useState<string | null>(null);
-
-  console.log(section);
 
   const teamId: number = tId;
   const retrospectiveId: number = rId;
@@ -69,21 +69,48 @@ const ActionItemTask: FC<ActionItemTaskProps> = ({ tId, rId, sId, section }) => 
     setSelectedUserName(name);
   };
 
+  const fetchRetrospectiveImage = async () => {
+    if (section.actionItems && section.actionItems.thumbnail) {
+      try {
+        const data = await postImageToS3({ filename: section.actionItems.thumbnail, method: 'GET' });
+        setSelectedUserImg(data.data.preSignedUrl);
+      } catch (e) {
+        toast.error('담당자 이미지 가져오기 실패');
+      }
+    } else {
+      setSelectedUserImg(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchRetrospectiveImage();
+  }, [ActionItems?.thumbnail]);
+
+  const renderImage = () => {
+    if (!ActionItems) {
+      return 'M';
+    }
+
+    if (ActionItems && selectedUserImg) {
+      return <img src={selectedUserImg} style={{ width: '30px' }} />;
+    }
+
+    if ((ActionItems && selectedUserImg === null) || selectedUserImg === '') {
+      return <CgProfile size="24px" color="#969696" />;
+    }
+  };
+
   return (
     <>
       <Popover isOpen={showPopup} onClose={() => setShowPopup(false)}>
         <PopoverTrigger>
-          <S.ManagerButton onClick={togglePopup}>
-            {selectedUserImg ? (
-              <img src={selectedUserImg} style={{ width: '30px' }} />
-            ) : selectedUserImg === '' ? (
-              <CgProfile size="24px" color="#969696" />
-            ) : (
-              'M'
-            )}
-          </S.ManagerButton>
+          <S.ManagerButton onClick={togglePopup}>{renderImage()}</S.ManagerButton>
         </PopoverTrigger>
-        {selectedUserName ? <S.ManagerText> {selectedUserName}</S.ManagerText> : <S.ManagerText>담당자</S.ManagerText>}
+        {section.actionItems ? (
+          <S.ManagerText> {selectedUserName}</S.ManagerText>
+        ) : (
+          <S.ManagerText>담당자</S.ManagerText>
+        )}
         <PopoverContent border={'none'}>
           <Members
             users={users}
