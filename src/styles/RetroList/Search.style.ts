16187495a19408eeb0d47aeb2786a7a25f8fc44a
yena <rladpsk1117@gmail.com>
2024-05-19 15:38:34 +0900

    :lipstick: Design: RetroList 검색창 삭제 버튼 수정

--- src/styles/RetroList/Search.style.ts
+++ src/styles/RetroList/Search.style.ts
@@ -18,8 +18,26 @@ export const SearchInput = styled.input`
   &::placeholder {
     color: #505f98;
   }
+  &::-ms-clear,
+  &::-ms-reveal {
+    display: none;
+  }
+  &::-webkit-search-decoration,
+  &::-webkit-search-cancel-button,
+  &::-webkit-search-results-button,
+  &::-webkit-search-results-decoration {
+    -webkit-appearance: none;
+    width: 15px;
+    height: 15px;
+    position: absolute;
+    right: 5%;
+    background-image: url('data:image/svg+xml;utf8,<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M400 145.1L366.9 112 256 222.9 145.1 112 112 145.1 222.9 256 112 366.9 145.1 400 256 289.1 366.9 400 400 366.9 289.1 256z"/></svg>');
+    background-repeat: no-repeat;
+    &:hover {
+      cursor: pointer;
+    }
+  }
 `;
-
 export const SearchIconImg = styled.img`
   position: absolute;
   left: 20px;
