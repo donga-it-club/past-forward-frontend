e9b2439108dfe0d4052a3fb1ef923c8630b17406
yena <rladpsk1117@gmail.com>
2024-05-21 00:36:40 +0900

    :sparkles: Feature: WriteRetroRevise 뒤로가기 버튼 생성

---
+++ src/styles/writeRetroStyles/BackButton.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  height: 33px;
+  padding: 5px;
+  margin-left: 35px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 600;
+  background-color: #eeeeee;
+  color: 'black';
+  &:hover {
+    cursor: pointer;
+  }
+`;
