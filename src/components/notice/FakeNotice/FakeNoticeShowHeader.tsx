fe1eb0bf696212220b127b1be5448f9bfdb015dd
geumbin <geumbin1204@gmail.com>
2024-07-29 16:23:22 +0900

    :sparkles: Feat: 게시물 종료 버튼 추가

--- src/components/notice/FakeNotice/FakeNoticeShowHeader.tsx
+++ src/components/notice/FakeNotice/FakeNoticeShowHeader.tsx
@@ -1,3 +1,6 @@
+import { MdOutlineClose } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+
 import * as S from '@/styles/notice/noticeShow.style';
 
 interface NoticeShowHeaderProps {
@@ -8,10 +11,21 @@ interface NoticeShowHeaderProps {
 
 export const FakeNoticeShowHeader = ({ title, date, view }: NoticeShowHeaderProps) => {
   const NoticeWriter = '관리자';
+  const navigate = useNavigate();
+  const handleNoticeClose = () => {
+    navigate('/');
+  };
 
   return (
     <>
       <S.NoticeShowHeaderStyle>
+        <button onClick={handleNoticeClose}>
+          <MdOutlineClose
+            style={{ position: 'absolute', top: '20px', right: '10vw' }}
+            size={34}
+            color="rgba(0, 0, 0, 0.2)"
+          />
+        </button>
         <p style={{ fontSize: '40px', fontWeight: '800', color: '#25213B', margin: '20px' }}>공지사항</p>
         {/* 게시물 제목 */}
         <S.NoticeShowTitle>{title}</S.NoticeShowTitle>
